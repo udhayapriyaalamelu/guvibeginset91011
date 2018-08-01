57e6c6bca2bf46a89ff87cc1987c1170ca0ce53b
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 17:57:22 +0530

    Add files via upload

---
+++ Pgm19.java
@@ -0,0 +1,14 @@
+package guvi_set911;
+import java.util.*;
+public class Pgm19
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+System.out.println((a*b)%c);
+	}
+
+}
