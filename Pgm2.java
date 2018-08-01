c0716d4a6d7b9f2c8ba39b8207b9d1209637adad
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 14:39:31 +0530

    Add files via upload

---
+++ Pgm2.java
@@ -0,0 +1,14 @@
+package guvi_set911;
+import java.util.*;
+public class Pgm2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+float a=sc.nextFloat();
+float b=sc.nextFloat();
+System.out.printf("%.5f",(a*b));
+sc.close();
+	}
+
+}
