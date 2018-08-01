9578641b2aec34816e4e07af05171989b8caa5a9
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 15:03:52 +0530

    Add files via upload

---
+++ Pgm4.java
@@ -0,0 +1,13 @@
+package guvi_set911;
+import java.util.*;
+public class Pgm4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+Character c=sc.nextLine().charAt(0);
+int value=c;
+System.out.println((int)c);
+	}
+
+}
