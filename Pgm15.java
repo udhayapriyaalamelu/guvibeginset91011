dc81e7f4f39eac515b82d68d4864947ff81774bb
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 17:10:47 +0530

    Add files via upload

---
+++ Pgm15.java
@@ -0,0 +1,16 @@
+package guvi_set911;
+import java.util.*;
+public class Pgm15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int p=sc.nextInt();
+int t=sc.nextInt();
+int r=sc.nextInt();
+int si=0;
+si=(p*t*r)/100;
+System.out.println(si);
+	}
+
+}
