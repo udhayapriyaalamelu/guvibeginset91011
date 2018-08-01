15a117ca8e4ed828aff415d951e5b6471143aa08
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 16:10:09 +0530

    Add files via upload

---
+++ Pgm8.java
@@ -0,0 +1,21 @@
+package guvi_set911;
+import java.util.*;
+public class Pgm8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int c=0;
+		for(int i=1;i<=n&&i<=m;i++)
+		{
+			if(n%i==0&&m%i==0)
+				c=i;
+		}
+		int lcm=(n*m)/c;
+		System.out.println(lcm);
+		
+	}
+
+}
