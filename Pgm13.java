35d82b3734d48ccf57c068aa156bf29968aa0cf0
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 16:49:33 +0530

    Add files via upload

---
+++ Pgm13.java
@@ -0,0 +1,14 @@
+package guvi_set911;
+import java.util.*;
+public class Pgm13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int temp=sc.nextInt();
+int kel=0;
+kel=(int) (temp+273.15);
+System.out.println(kel);
+	}
+
+}
