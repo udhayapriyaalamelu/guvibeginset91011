bbcd50149b84d0750360b15e6cca897acc1240a4
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 18:48:26 +0530

    Add files via upload

--- Pgm1.java
+++ Pgm1.java
@@ -1,16 +1,18 @@
-package guvi_set911;
+package guviplayset123;
 import java.util.*;
 public class Pgm1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner sc=new Scanner(System.in);
-int a=sc.nextInt();
-int b=sc.nextInt();
-int c=0;
-if(a<b)
-	c=Math.abs(a-b);
-	System.out.println(c+" ");
+String s=sc.nextLine();
+String rev="";
+int len=s.length();
+for(int i=len-1;i>=0;i--)
+{
+	rev=rev+s.charAt(i);
+}
+System.out.println(rev);
 	}
 
 }
